6e225e5117f93457f26cf7dc3cb9a386cc5b7821
RED6908 <2202035@utrivieramaya.edu.mx>
2024-02-02 11:29:19 -0500

    first commit

---
+++ Juego.php
@@ -0,0 +1,27 @@
+
+<?php
+$xmlstr = <<<XML
+<?xml version='1.0' standalone='yes'?>
+<videojuegos>
+ <videojuegos>
+  <titulo>Lead 4 Dead</titulo>
+  <personajes>
+   <personaje>
+    <titulo>Halo Reach</nombre>
+   </personaje>
+   <personaje>
+    <titulo>Residen Evil 4</nombre>
+   </personaje>
+  </personajes>
+  <argumento>
+	Estos son los 3 títulos que fueron bien recibidos por la critica
+  </argumento>
+  <grandes-frases>
+   <frase>Top 3 de los mejores videojuegos</frase>
+  </grandes-frases>
+  <puntuacion tipo="votos">7</puntuacion>
+  <puntuacion tipo="estrellas">5</puntuacion>
+ </videojuegos>
+</videojuegos>
+XML;
+?>
